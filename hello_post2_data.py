b03d750083e822a83c66fa09a433769fe10921f8
seung@devbox20 <seung@devbox20>
2022-05-25 22:56:18 +0900

    updated inference code

--- hello_post2_data.py
+++ hello_post2_data.py
@@ -36,11 +36,9 @@ def send_post():
 
 @app.route('/send_post2', methods=['GET'])
 def send_post2():
-    from tensorflow.keras.models import load_model
-    t0 = time.time()
-    fn_model = './models/08_walking_wearable21_20210823/model_08_walking_wearable21_20210823_Conv1D_SMPL56500_EP240'
-    model = load_model(fn_model)
-    t1 = time.time()-t0
+    global model, t1
+    
+    
     #fn_pickle = './pickle/08_walking_wearable21_20210823_class18_len150_pub.pickle'
     fn_pickle = './pickle/08_walking_wearable21_20210823_class18_len100_pub.pickle'
 
@@ -51,23 +49,31 @@ def send_post2():
     n_layers = len(model.layers)
     
     idx_to_draw = np.random.randint(0, data_test.shape[0] - 1)
-    idx_to_draw = 14065
+    #idx_to_draw = 14065
 
     X = data_test[idx_to_draw]
     #x0 = np.expand_dims( testX[0], 0)
     X = np.expand_dims( X, 0)
     y = labels_test[idx_to_draw]
     y_argmax = np.argmax(y, axis=0)
+    t0 = time.time()
     y_pred = model.predict(X)[0]
+    dt_pred = time.time() - t0
+
     y_pred_argmax = np.argmax(y_pred, axis =0)
     y_pred_proba = y_pred[y_pred_argmax]
 
     # https://www.w3schools.com/python/ref_requests_post.asp
 
     if True:
-        myobj = {'n_layers':n_layers, 't1':t1,'xshape':np.shape(X), 
+        myobj = {'n_layers':n_layers, 
+                'dt_load':'{:.3f}'.format(t1),
+                'dt_pred':'{:.3f}'.format(dt_pred),
+                'x_idx':str(idx_to_draw),
+                'xshape':np.shape(X), 
                 'y':y.tolist(), 
                 'y_pred':y_pred.tolist(), 
+                
                 'y_argmax': str(y_argmax),
                 'y_pred_argmax':str(y_pred_argmax),
                 'y_pred_proba': '{:.3f}'.format(y_pred_proba)
@@ -87,9 +93,25 @@ def send_post2():
     #res = requests.post(url, data=json.dumps(params))
     return res.text
 
+from tensorflow.keras.models import load_model
 
+
+def setup_app(app):
+    global model, t1
+   # All your initialization code
+    t0 = time.time()
+    fn_model = './models/08_walking_wearable21_20210823/model_08_walking_wearable21_20210823_Conv1D_SMPL56500_EP240'
+    model = load_model(fn_model)
+    t1 = time.time()-t0
+
+setup_app(app)
 
 if __name__ == '__main__':
-    app.debug = True
+    
+    app.debug = False#True
+    
     #app.run()
+    
+    
+
     app.run(host='0.0.0.0', port=81)
